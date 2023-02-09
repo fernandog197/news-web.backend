e6d95d4b78b779abdcb062d8903d8272467239fd
fernandog197 <bluewavesalta@gmail.com>
2023-02-09 12:04:49 -0300

    Adding security packages

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 import * as dotenv from 'dotenv'
+
 import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
+
+//Security packages 
 import cors from 'cors'
+import helmet from 'helmet'
+import xss from 'xss-clean'
+import rateLimiter from 'express-rate-limit'
+
 import morgan from 'morgan'
 
 import allRouters from './src/routes/index.js'
@@ -10,10 +17,17 @@ import allRouters from './src/routes/index.js'
 dotenv.config()
 const app = express()
 
+app.set('trust proxy', 1)
+app.use(rateLimiter({
+  windowMs: 15*60*1000,
+  max: 100,
+}))
 app.use(bodyParser.urlencoded({ limit:'50mb', extended: true }))
 app.use(bodyParser.json({ limit:'50mb', extended: true }))
 app.use(cors())
 app.use(morgan('dev'))
+app.use(helmet())
+app.use(xss())
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Credentials', true)
